d5c0d0071ab3c0247f652298049781995817d260
Chen Yue <liveforexperience@126.com>
2026-03-29 23:26:42 +0800

    fix(ui): use relative paths in tailwind.config.js content to fix missing styles on server

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,8 @@ import path from 'path';
 export default {
   darkMode: 'class',
   content: [
-    "/Users/chenyue/develop/my_own_work/oldboy-club-manager/frontend/index.html",
-    "/Users/chenyue/develop/my_own_work/oldboy-club-manager/frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
